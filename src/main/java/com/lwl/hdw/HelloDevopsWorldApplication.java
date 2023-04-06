ae9bcdb3ada891207f157fea0ddea692ac8d5a8d
javawithlwl <javawithlwl@gmail.com>
2023-04-06 17:16:46 +0530

    initial commit

---
+++ src/main/java/com/lwl/hdw/HelloDevopsWorldApplication.java
@@ -0,0 +1,15 @@
+package com.lwl.hdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevopsWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDevopsWorldApplication.class, args);
+	}
+
+	
+
+}
